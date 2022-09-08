63eb5a69615df7d006351825ef4d73b521fa684d
chanan-ru <chananchida.ru@gmail.com>
2022-09-08 22:08:58 +1000

    Added conditions to display.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,31 @@
 //This array contains Pokémon data to display in application.
-let pokemonList = [
-  {
-    name : 'Charmander',
-    height : 0.6,
-    types : ['Fire']
+let pokemonList = [{
+    name: 'Charmander',
+    height: 0.6,
+    types: ['Fire']
   },
 
   {
-    name : 'Growlithe',
-    height : 0.7,
-    types : ['Fire']
+    name: 'Growlithe',
+    height: 0.7,
+    types: ['Fire']
   },
 
   {
-    name : 'Lapras',
-    height : 2.5,
-    types : ['Ice', 'Water']
+    name: 'Lapras',
+    height: 2.5,
+    types: ['Ice', 'Water']
   }
 ];
 
 
-document.write(pokemonList[2].name + ' ' + pokemonList[2].types);
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].name != '' && pokemonList[i].height >= 1) {
+    document.write('<p>' + pokemonList[i].name + ' (' + pokemonList[i].types + ') is ' + pokemonList[i].height + 'm height. -- Wow!!, that\'s big.</p>');
+  } else if (pokemonList[i].name != '' && pokemonList[i].height > 0) {
+    document.write('<p>' + pokemonList[i].name + ' (' + pokemonList[i].types + ') is ' + pokemonList[i].height + 'm height. </p>');
+  } else {
+    document.write('<p>Error!!, Please check your details.</p>');
+  }
+
+}
