fd538e1fac26d81b8eb98e1ad10b07c0075ba28b
chanan-ru <chananchida.ru@gmail.com>
2022-09-17 19:44:34 +1000

    wrapped variables within an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,49 @@
-//This array contains Pokémon data to display in application.
-let pokemonList = [{
-    name: 'Charmander',
-    height: 0.6,
-    types: ['Fire']
-  },
+// IIFE
+let pokemonRepository = (function() {
+  let pokemonList = [{
+      name: 'Charmander',
+      height: 0.6,
+      types: ['Fire']
+    },
 
-  {
-    name: 'Growlithe',
-    height: 0.7,
-    types: ['Fire']
-  },
+    {
+      name: 'Growlithe',
+      height: 0.7,
+      types: ['Fire']
+    },
 
-  {
-    name: 'Lapras',
-    height: 2.5,
-    types: ['Ice', 'Water']
-  }
-];
+    {
+      name: 'Lapras',
+      height: 2.5,
+      types: ['Ice', 'Water']
+    }
+  ];
 
-
-/* An example of using For loop
-
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].name != '' && pokemonList[i].height >= 1) {
-    document.write('<p>' + pokemonList[i].name + ' (' + pokemonList[i].types + ') is ' + pokemonList[i].height + 'm height. -- Wow!!, that\'s big.</p>');
-  } else if (pokemonList[i].name != '' && pokemonList[i].height > 0) {
-    document.write('<p>' + pokemonList[i].name + ' (' + pokemonList[i].types + ') is ' + pokemonList[i].height + 'm height. </p>');
-  } else {
-    document.write('<p>Error!!, Please check your details.</p>');
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
 
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+
+})();
+
+pokemonRepository.add(
+  {
+  name: 'Bulbasaur',
+  height: 0.7,
+  types: ['Grass', 'Poison']
 }
+);
 
-*/
-
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
 
   document.write('<p>' + pokemon.name + '</p>');
-  
+
 });
