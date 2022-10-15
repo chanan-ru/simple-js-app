541c385fb2306e6ba514f79c0b009c69d7d7f9ff
chanan-ru <chananchida.ru@gmail.com>
2022-10-15 21:33:04 +1100

    Edited API link

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // IIFE
 let pokemonRepository = (function() {
   let pokemonList = [];
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=1000';
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=10';
 
   //Variable for Modal
   let modalContainer = document.querySelector('.modal-container');
